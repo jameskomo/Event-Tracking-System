49e2f4391db1ecea6832a18267ed77729f786c04
jameskomo <james.komoh@gmail.com>
2019-07-18 15:33:49 +0300

    new constant db defined in event.js that allows HTTP request methods to be defined including endpoints

--- event.js
+++ event.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express()
+const db = require('./queries')
 const port = 3000
 
 app.use(bodyParser.json())
@@ -16,6 +17,12 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'This is an event tracker based on Node.js, Express, and Postgres API' })
   });
+
+  app.get('/tasks', db.getTasks)
+  app.get('/tasks/:id', db.getTaskById)
+  app.post('/tasks', db.createTask)
+  app.put('/tasks/:id', db.updateTask)
+  app.delete('/tasks/:id', db.deleteTask)
 
 
 app.listen(port, () => {
