1b4abe6f8e3d5c0c4eb1eb50d4416943caa601ac
jameskomo <james.komoh@gmail.com>
2019-07-18 15:12:47 +0300

    GET task Request created that allows querying of DB for specific task by ID with pool query defined for querying DB

--- query.js
+++ query.js
@@ -19,4 +19,14 @@ const getTasks = (request, response) => {
     })
   }
 
+  const getTaskById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM task WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
   
