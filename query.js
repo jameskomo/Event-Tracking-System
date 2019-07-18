adab3c4c4925024b9dd5a989baaa0d066de78c1c
jameskomo <james.komoh@gmail.com>
2019-07-18 15:10:56 +0300

    GET Request created that allows querying of DB for all tasks with pool query defined for querying DB

--- query.js
+++ query.js
@@ -8,3 +8,15 @@ const pool = new Pool({
   password: 'event',
   port: 5432,
 })
+
+
+const getTasks = (request, response) => {
+    pool.query('SELECT * FROM task ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  
