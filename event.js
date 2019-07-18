bdfc3fbc04ffa51dcc394234a06ce6a74b46d40c
jameskomo <james.komoh@gmail.com>
2019-07-18 16:58:58 +0300

    node schedule package installed for handling cron jobs and imported in entry file

--- event.js
+++ event.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var schedule = require('node-schedule');
 
 const app = express()
 const db = require('./query')
@@ -16,6 +17,7 @@ app.use(
 
 app.get('/', (request, response) => {
     response.json({ info: 'This is an event tracker based on Node.js, Express, and Postgres API' })
+
   });
 
   app.get('/tasks', db.getTasks)
@@ -23,6 +25,48 @@ app.get('/', (request, response) => {
   app.post('/tasks', db.createTask)
   app.put('/tasks/:id', db.updateTask)
   app.delete('/tasks/:id', db.deleteTask)
+
+
+
+// Setting up cron jobs for Task StaticRange, stop and reporting based on *second    *minute    *hour    * day of month   *day    *day of week(0 or7 sunday to)
+
+
+  var start_tasks = schedule.scheduleJob('01 * * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
+    // Starting Servers
+    function randomServersStart(min,max) // min and max included
+    {
+        return Math.floor(Math.random()*(max-min+1)+min);
+        var start_servers=randomServersStart(10, 20);
+        console.log("Start " + start_servers + " servers");
+        
+    }
+    
+      // Task ends
+
+
+  // Stopping Servers
+  function randomServersStop(min,max) // min and max included
+  {
+      return Math.floor(Math.random()*(max-min+1)+min);
+      var stop_servers=randomServersStart(5, start_servers);
+      console.log("Start " + start_servers + " servers");
+      
+  }
+  // Task ends
+
+  // Task Reporting
+    var server_reporting=start_servers;
+ 
+  // Task ends
+
+  
+  });
+
+// Cron job end
+
+
+
 
 
 app.listen(port, () => {
