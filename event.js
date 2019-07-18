53e1a2c5462e745fff82614c299c8421fc20815d
jameskomo <james.komoh@gmail.com>
2019-07-18 14:45:27 +0300

    PostgreSQL DB created, task table with fields created, pg installed to project dependencies and Node boilerplate created

---
+++ event.js
@@ -0,0 +1,13 @@
+//jshint esversion: 6
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
